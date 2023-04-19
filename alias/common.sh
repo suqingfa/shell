d06ef7a81f6c4ca7f3f37f65c1f3eb274da5f2c3
苏 <enmoton@qq.com>
2023-04-19 13:40:06 +0800

    common

--- alias/common.sh
+++ alias/common.sh
@@ -5,7 +5,10 @@ alias df='df -h'
 
 alias type='type -a'
 
+alias hexdump='hexdump --canonical --length=0x200'
+
 alias readelf='readelf --wide'
+alias objdump='objdump --wide'
 
 alias ls='ls --color=auto'
 alias ll='ls -lh --color=auto'
@@ -17,22 +20,22 @@ alias grep='grep --color=auto'
 
 alias ping='ping -c 10'
 
-
-if $(type pacman &> /dev/null); then
+if $(type yay &> /dev/null); then
+	alias upgrade='yay --sync --sysupgrade --refresh --noconfirm'
+	alias install='yay --sync --sysupgrade'
+	alias remove='yay --remove --recursive'
+	alias contents='yay --query --list'
+elif $(type pacman &> /dev/null); then
 	alias upgrade='sudo pacman --sync --sysupgrade --refresh --noconfirm'
-	alias install='sudo pacman --sync'
+	alias install='sudo pacman --sync --sysupgrade'
 	alias remove='sudo pacman --remove --recursive'
 	alias contents='pacman --query --list'
-fi
-
-if $(type apt-get &> /dev/null); then
+elif $(type apt-get &> /dev/null); then
 	alias upgrade='sudo apt-get update && sudo apt-get upgrade -y'
 	alias install='sudo apt-get install'
 	alias remove='sudo apt-get remove'
 	alias contents='apt-file list'
-fi
-
-if $(type brew &> /dev/null); then
+elif $(type brew &> /dev/null); then
 	alias upgrade='brew update && brew upgrade'
 	alias install='brew install'
 	alias remove='brew uninstall'

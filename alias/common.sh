07321a3bd55489480dcb6ae2311b243ed2956c3a
苏 <root@suqf.top>
2023-10-23 20:15:33 +0800

    purge

--- alias/common.sh
+++ alias/common.sh
@@ -10,31 +10,27 @@ alias hexdump='hexdump --canonical --length=0x200'
 alias readelf='readelf --wide'
 alias objdump='objdump --wide'
 
-alias ls='ls --color=auto'
-alias ll='ls -lh --color=auto'
-alias la='ls -lha --color=auto'
-
-alias ..='cd ..'
-
 alias grep='grep --color=auto'
 
 alias ping='ping -c 10'
 
-if $(type yay &> /dev/null); then
-	alias upgrade='yay --sync --sysupgrade --refresh --noconfirm'
-	alias install='yay --sync --sysupgrade'
-	alias remove='yay --remove --recursive'
-	alias contents='yay --query --list'
-elif $(type pacman &> /dev/null); then
+alias last='last --time-format=iso --limit=10'
+
+alias journalctl='journalctl --output=short-iso'
+
+if $(type pacman &> /dev/null); then
 	alias upgrade='sudo pacman --sync --sysupgrade --refresh --noconfirm'
 	alias install='sudo pacman --sync --sysupgrade'
-	alias remove='sudo pacman --remove --recursive'
-	alias contents='pacman --query --list'
+	alias remove='sudo pacman --remove --recursive --nosave'
+	alias purge='pacman --query --unrequired --deps --quiet | sudo pacman --remove --recursive --nosave -'
+	alias contents='pacman --query --color auto --list'
+	alias contents-owns='pacman --query --color auto --owns'
 elif $(type apt-get &> /dev/null); then
 	alias upgrade='sudo apt-get update && sudo apt-get upgrade -y'
 	alias install='sudo apt-get install'
 	alias remove='sudo apt-get remove'
 	alias contents='apt-file list'
+	alias contents-owns='apt-file find'
 elif $(type brew &> /dev/null); then
 	alias upgrade='brew update && brew upgrade'
 	alias install='brew install'
